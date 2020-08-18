1e8f62f4b01f01b124f51784cfa9087c90835d71
aleiWisdom <aleiwisdom@gmail.com>
2020-08-18 14:10:22 +0800

    添加了文件信息

--- ChangedUIOfPSOnline.js
+++ ChangedUIOfPSOnline.js
@@ -1,3 +1,7 @@
+//此代码使用环境为油猴插件
+//地址:https://www.uupoop.com/
+//作用:去掉该网页上方大黑条  \(@^0^@)/
+
 (function () {
     'use strict';
     document.getElementsByClassName("page-header")[0].remove();
--- DeleteBottomOfBing.js
+++ DeleteBottomOfBing.js
@@ -1,3 +1,7 @@
+//此代码使用环境为油猴插件
+//地址:https://www.bing.com/
+//作用:去掉该网页下方备案信息大黑条:D
+
 (function () {
     'use strict';
     document.getElementById("b_footer").hidden = true;
